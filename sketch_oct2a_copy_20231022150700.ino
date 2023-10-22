06b9f5e421902ff605d11b96ac3a78ce704a2e5d
Yasdormiani <146694388+Yasdormiani@users.noreply.github.com>
2023-10-22 15:21:44 +0330

    Add files via upload

---
+++ sketch_oct2a_copy_20231022150700.ino
@@ -0,0 +1,16 @@
+int led = 13;
+
+void setup(){
+pinMode(led , OUTPUT);
+}
+
+void loop (){ 
+
+//for making flashing light
+
+digitalWrite (led  , HIGH);h
+delay(1000);
+digitalWrite (led , LOW);
+delay(1000);
+
+}
